837325ab52b436f899929990d7c259577986a629
alnesterenko <alzpupkin@gmail.com>
2023-03-30 17:23:15 +0300

    Разные типы переменных и их значения

--- src/main/java/stepik_java_summercamp_1/Main.java
+++ src/main/java/stepik_java_summercamp_1/Main.java
@@ -1,87 +1,20 @@
 package stepik_java_summercamp_1;
 
-import java.io.IOException;
-import java.util.*;
-import java.math.*;
-import java.util.Random;
+import java.time.LocalDateTime;
+import java.util.Scanner;
 
-import static java.lang.Integer.parseInt;
+public class Main{
+    public static void main(String[] args) {
 
-public class Main {
-
-    public static void main(String[] args) throws IOException {
-
-//        Иванов Иван +38056
-//        Столяров Сергей +38057
-//        Стоянов Илья +38055
-//        Сергей Дурдачник +38033
-
-//        Scanner scanner = new Scanner(System.in);
-//        TreeMap <String, TreeSet<String>> map = new TreeMap<>();
-//
-//        for (int i = 0; i < 4; i++) {
-//            String s = scanner.nextLine();
-//            String [] arrString = s.split(" ");
-//            String name = arrString[0] + " " + arrString[1];
-//            String number = arrString[2];
-//            if (map.containsKey(name)){
-//                map.get(name).add(number);
-//            }else {
-//                TreeSet<String> set = new TreeSet<>();
-//                set.add(number);
-//                map.put(name, set);
-//            }
-//        }
-//        for (String name :
-//                map.keySet()) {
-//            System.out.print(name + " : ");
-//            for (String number: map.get(name)){
-//                System.out.print(number + " ");
-//            }
-//            System.out.println();
-//        }
-
-        Scanner scanner = new Scanner(System.in);
-        TreeMap<String, TreeSet<String>> map = new TreeMap<>();
-        String [] arrNewFriends = {};
-
-        while (true){
-            String line = scanner.nextLine().trim();
-            if (line.isEmpty()){
-                break;
-            }else {
-                arrNewFriends = line.split(" ");
-                String firstFriend = arrNewFriends[0];
-                String secondFriend = arrNewFriends[1];
-                if (map.containsKey(firstFriend)){
-                    map.get(firstFriend).add(secondFriend);
-                }else{
-                    TreeSet<String> set = new TreeSet<>();
-                    set.add(secondFriend);
-                    map.put(firstFriend, set);
-                }
-                //А теперь друзей меняем местами
-                if (map.containsKey(secondFriend)){
-                    map.get(secondFriend).add(firstFriend);
-                }else{
-                    TreeSet<String> set = new TreeSet<>();
-                    set.add(firstFriend);
-                    map.put(secondFriend, set);
-                }
-
-
-            }
-        }
-        for (String name : map.keySet()) {
-            System.out.print(name + " Дружит с :");
-            for (String frend: map.get(name)) {
-                System.out.print(" " + frend);
-            }
-            System.out.println();
-        }
-
-
+        byte age = 121;
+        char gender = 'M';
+        float height = 1.76f;
+        short numberOfBricks = 19348;
+        double numberOfBacteria = 9.18E+09;
+        double distanceToNearestStar = 4.017E+13; // имеется в виду не Солнце, а другая звезда))))
+        boolean workPermit = true;
+        long fileSize = 6870896640L; // Фильм "Аватар 2" в хорошем качестве в формате mkv ))))
     }
-
 }
+
 
