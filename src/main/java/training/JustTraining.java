5ba5b855cd6cd7f8e9ec202aeb1c71dac4d7fbca
alnesterenko <alzpupkin@gmail.com>
2024-10-27 09:29:09 +0200

    temp_commit_

--- src/main/java/training/JustTraining.java
+++ src/main/java/training/JustTraining.java
@@ -4,117 +4,41 @@ import java.io.*;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class JustTraining {
 
-    public record Line(String level, String threads, String text) {
-    }
-
-    public Line parse(String text) {
-        int openingBrackets = 0, closingBrackets = 0, positiEndThreads = 0;
-        String level = "";
-        Pattern pattern = Pattern.compile("[A-Z]+");
-        Matcher matcher = pattern.matcher(text);
-        if (matcher.find()) {
-            level = text.substring(matcher.start(), matcher.end());
-        }
-        int positionOfFirstSpace = text.indexOf(' ');
-        char tempSymbol = ' ';
-        for (int i = positionOfFirstSpace + 1; i < text.length(); i++) {
-            tempSymbol = text.charAt(i);
-            if (tempSymbol == '[') {
-                openingBrackets++;
-            }
-            if (tempSymbol == ']') {
-                closingBrackets++;
-            }
-            if (openingBrackets == closingBrackets && openingBrackets > 0) {
-                positiEndThreads = i;
-                break;
-            }
-        }
-        return new Line(level,
-                text.substring(positionOfFirstSpace + 1, positiEndThreads + 1),
-                text.substring(positiEndThreads + 2));
-    }
-
-    public static void main(String[] args) throws IOException {
-        /*JustTraining training = new JustTraining();
-        System.out.println(training.parse("WARNING [threads [zqfz [wbye [zfq]]] - 1] wqqa [e] zqw"));*/
-        FindInLineDemo.main(args);
-    }
-
-    class AvgFile {
-        public static void main(String[] args) throws IOException {
-            int count = 0;
-            double sum = 0.0;
-
-            // Записать выходные данные в файл.
-            FileWriter fout = new FileWriter("text.txt");
-            fout.write("2 3.4 5 6 7.4 9.1 10.5 done");
-            fout.close();
-
-            FileReader fin = new FileReader("test.txt");
-            Scanner src = new Scanner(fin);
-
-            // Читать и суммировать числа.
-            while (src.hasNext()) {
-                if (src.hasNextDouble()) {
-                    sum += src.nextDouble();
-                    count++;
-                } else {
-                    String str = src.next();
-                    if ("done".equals(str)) {
-                        break;
-                    } else {
-                        System.out.println("Ошибка формата файла.");
-                        return;
-                    }
+    public boolean checkInclusion(String s1, String s2) {
+        boolean result = false;
+        if ((s1.equals(s2)) || (s1.length() == 1 && s2.indexOf(s1) != -1)) {
+            result = true;
+        } else {
+            List<Character> s1Set = s1.chars()
+                    .mapToObj(c -> (char) c)
+                    .collect(Collectors.toCollection(ArrayList::new));
+            int s1Length = s1.length();
+            int workingLength = s2.length() - s1Length + 1;
+            for (int i = 0; i < workingLength; i++) {
+                String window = s2.substring(i, i + s1Length);
+                if (isIdenticalSet(window, s1Set) && !s1.equals(window)) {
+                    result = true;
+                    break;
                 }
             }
-            src.close();
-            System.out.println("Среднее значение равно " + sum / count);
         }
+        return result;
     }
 
-    class ScanMixed {
-        public static void main(String[] args) throws IOException {
-            // Запись входных данных в файл.
-            FileWriter fout = new FileWriter("stepik_java_summercamp_1_1/src/data/test.txt");
-            fout.write("Testing;Scanner;10;12.2\none;true;two;false");
-            fout.close();
-
-            FileReader fin = new FileReader("stepik_java_summercamp_1_1/src/data/test.txt");
-            Scanner src = new Scanner(fin);
-
-            // Читать до конца.
-            while (src.hasNextLine()) {
-                String oneLine = src.nextLine();
-                Scanner oneWord = new Scanner(oneLine);
-                oneWord.useDelimiter(";");
-                while (oneWord.hasNext()) {
-                    System.out.println(oneWord.next());
-                }
-                oneWord.close();
-                System.out.println("NextLine: ");
-            }
-            src.close();
-        }
+    private boolean isIdenticalSet(String window, List<Character> s1Set) {
+        List<Character> windowSet = window.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.toCollection(ArrayList::new));
+        return windowSet.equals(s1Set);
     }
 
-    class FindInLineDemo {
-        public static void main(String[] args) {
-            String instr = "Name: Tom Age: 28 ID: 77";
-            Scanner conin = new Scanner(instr);
-            // Найти и отобразить возраст.
-            conin.findInLine("Age:"); // Найти Age
-            if (conin.hasNext()) {
-                System.out.println(conin.next());
-            } else {
-                System.out.println("Ошибка!");
-            }
-            conin.close();
-        }
+    public static void main(String[] args) {
+        JustTraining test = new JustTraining();
+        System.out.println(test.checkInclusion("hello", "ooolleoooleh"));
+        System.out.println(test.checkInclusion("a", "ab"));
     }
-
 }
