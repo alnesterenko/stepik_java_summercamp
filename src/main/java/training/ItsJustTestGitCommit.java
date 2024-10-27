f0d4d7b6f8a28c2e08dc61ccb8761062d05b9f1a
alnesterenko <alzpupkin@gmail.com>
2024-10-27 10:09:38 +0200

    проверка работы Гита_

--- src/main/java/training/ItsJustTestGitCommit.java
+++ src/main/java/training/ItsJustTestGitCommit.java
@@ -2,6 +2,6 @@ package training;
 
 public class ItsJustTestGitCommit {
     public static void main(String[] args) {
-        System.out.println("test! test!");
+        System.out.println("test!");
     }
 }
