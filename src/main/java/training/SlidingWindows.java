d4413e10b21d816a4335dc6483ca04a9d5d51b75
alnesterenko <alzpupkin@gmail.com>
2024-10-30 17:46:40 +0200

    Задача про нахождение максимума в скользящем окне

--- src/main/java/training/SlidingWindows.java
+++ src/main/java/training/SlidingWindows.java
@@ -5,44 +5,40 @@ import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringJoiner;
 
 public class SlidingWindows {
 
-    public double[] medianSlidingWindow(int[] nums, int k) {
-        List<Double> resultList = new ArrayList<>();
-        int[] slidingWindowArr = new int[k];
-        for (int i = 0; i < nums.length - (k - 1); i++) {
-            System.arraycopy(nums, i, slidingWindowArr, 0, k);
-            Arrays.sort(slidingWindowArr);
-            resultList.add(toBigDecimalAndBack(slidingWindowArr, k));
-        }
-        return resultList.stream().mapToDouble(i -> i).toArray();
-    }
-
-    private double toBigDecimalAndBack(int[] slidingWindowArr, int k) {
-        BigDecimal resultBD;
-        if (k % 2 == 0) {
-            resultBD = new BigDecimal((slidingWindowArr[slidingWindowArr.length / 2 - 1] / 2) + (slidingWindowArr[slidingWindowArr.length / 2] / 2));
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int windowMax;
+        int numLength = nums.length;
+        int lengthOfResultArr = numLength - k + 1; // вычисление размера результирующего массива
+        int[] resultArr = new int[lengthOfResultArr];
+        if (numLength == 0 || k == 0) {
+            resultArr = new int[]{nums[0]};
+        } else if (numLength == 1) {
+            resultArr = nums;
         } else {
-            resultBD = new BigDecimal(slidingWindowArr[slidingWindowArr.length / 2]);
+            for (int i = 0; i < numLength - k + 1; i++) {
+                windowMax = nums[i];
+                for (int j = i; j < k + i; j++) {
+                   windowMax = Math.max(windowMax, nums[j]);
+                }
+                resultArr[i] = windowMax;
+            }
         }
-        resultBD = resultBD.setScale(5, RoundingMode.DOWN);
-        return resultBD.doubleValue();
+        return resultArr;
     }
 
     public static void main(String[] args) {
-       int[] nums = {2147483647, 2147483647};
-       int k = 2;
-       SlidingWindows slidingWindows = new SlidingWindows();
-       double[] result = slidingWindows.medianSlidingWindow(nums, k);
-        for (double oneDigite : result) {
-            System.out.println(oneDigite);
+        int[] nums = {1, 8, 9, 9, 5, 6, 7, 8, 9}; // старый ответ 24
+        int k = 3;
+        SlidingWindows slidingWindows = new SlidingWindows();
+        int[] resultArr = slidingWindows.maxSlidingWindow(nums, k);
+        StringJoiner stringJoiner = new StringJoiner(", ", "[", "]");
+        for (int oneDigit : resultArr) {
+            stringJoiner.add(String.valueOf(oneDigit));
         }
-        /*System.out.println("------------------------");
-        double value = 1.0;
-        BigDecimal resultBD = new BigDecimal(value);
-        resultBD = resultBD.setScale(5, RoundingMode.DOWN);
-        System.out.println(resultBD);
-        System.out.println(resultBD.doubleValue());*/
+        System.out.println(stringJoiner.toString());
     }
 }
