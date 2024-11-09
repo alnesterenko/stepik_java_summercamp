27a1dacce608ead42d6c88af323c75b6ab2acd76
alnesterenko <alzpupkin@gmail.com>
2024-11-09 20:40:49 +0200

    Ещё более жадный алгоритм )))

--- src/main/java/training/GreedyAlgorithm.java
+++ src/main/java/training/GreedyAlgorithm.java
@@ -3,32 +3,44 @@ package training;
 public class GreedyAlgorithm {
 
     public int maxProfit(int[] prices) {
+        int buyPrice = prices[0];
         int maxProfit = 0;
+        int globalMaxProfit = 0;
         int pricesLength = prices.length;
-        if (pricesLength >= 2) {
-            for (int i = 0; i < pricesLength - 1; i++) {
-                for (int j = i + 1; j < pricesLength; j++) {
-                    if (prices[j] - prices[i] > maxProfit) {
-                        maxProfit = prices[j] - prices[i];
-                    }
+        for (int i = 1; i < pricesLength; i++) {
+            if (prices[i] < buyPrice) {
+                buyPrice = prices[i];
+            } else if (prices[i] - buyPrice > maxProfit) {
+                maxProfit = prices[i] - buyPrice;
+            }
+        }
+        globalMaxProfit = Math.max(globalMaxProfit, maxProfit);
+        buyPrice = prices[0];
+        maxProfit = 0;
+        for (int i = 1; i < pricesLength; i++) {
+            if (prices[i] < buyPrice) {
+                buyPrice = prices[i];
+            } else if (prices[i] - buyPrice > 0) {
+                maxProfit += prices[i] - buyPrice;
+                if (i < pricesLength - 1) {
+                    buyPrice = prices[i + 1];
                 }
             }
         }
-        return maxProfit;
+        globalMaxProfit = Math.max(globalMaxProfit, maxProfit);
+        return globalMaxProfit;
     }
 
     public static void main(String[] args) {
         GreedyAlgorithm greedyAlgorithm = new GreedyAlgorithm();
         int[] prices = {7, 1, 5, 3, 6, 4};
-        int expected = 5;
+        int expected = 7;
         int result = greedyAlgorithm.maxProfit(prices);
-        boolean equals = expected == result;
-        System.out.println("Результат соответствует ожидаемому значению: " + equals);
+        System.out.println("Ожидаемое значение: " + expected + " Полученный результат: " + result);
         System.out.println("===========================================");
-        prices = new int[]{2, 4, 1};
-        expected = 2;
+        prices = new int[] {7, 6, 4, 3, 1};
+        expected = 0;
         result = greedyAlgorithm.maxProfit(prices);
-        equals = expected == result;
-        System.out.println("Результат соответствует ожидаемому значению: " + equals);
+        System.out.println("Ожидаемое значение: " + expected + " Полученный результат: " + result);
     }
 }
