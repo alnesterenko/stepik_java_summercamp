20829ac54bee9484ecdc99071b08e6421e2b67bc
alnesterenko <alzpupkin@gmail.com>
2025-04-25 17:22:46 +0300

    Три потока соревнуются, но победитель лишь один. Причёсанная версия.

--- src/main/java/training/ThreeThreads.java
+++ src/main/java/training/ThreeThreads.java
@@ -1,86 +1,54 @@
 package training;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ThreeThreads {
-    public static AtomicBoolean haveWinner = new AtomicBoolean(false);
+    public static volatile AtomicBoolean haveWinner = new AtomicBoolean(false);
     public static String nameOfWinner = null;
     public static ThreadLocal<Integer> pause = new ThreadLocal<>();
-    public static final int COUNT = 1_000;
+    public static final int COUNT = 100;
 
     public static void main(String[] args) {
-        Thread first = new Thread(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        pause.set(1);
-                        int localCount = COUNT;
-                        while (!haveWinner.get() && localCount != 0) {
-                            try {
-                                Thread.sleep(pause.get());
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
+        List<Thread> threadList = new ArrayList<>();
+        for (int i = 3; i > 0; i--) {
+            int finalI = i;
+            threadList.add(
+                    new Thread(
+                            new Runnable() {
+                                @Override
+                                public void run() {
+                                    pause.set(finalI);
+                                    int localCount = COUNT;
+                                    while (!haveWinner.get() && localCount != 0) {
+                                        try {
+                                            Thread.sleep(pause.get());
+                                        } catch (InterruptedException e) {
+                                            e.printStackTrace();
+                                        }
+                                        localCount--;
+                                    }
+                                    if (haveWinner.compareAndSet(false, true)) {
+                                        nameOfWinner = Thread.currentThread().getName();
+                                        System.out.println("!!! haveWinner: " + haveWinner.get()
+                                                + " поток " + Thread.currentThread().getName());
+                                    }
+                                }
                             }
-                            localCount--;
-                        }
-                        if (haveWinner.compareAndSet(false, true)) {
-                            nameOfWinner = Thread.currentThread().getName();
-                        }
-                    }
-                }
-        );
-        Thread second = new Thread(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        pause.set(1);
-                        int localCount = COUNT;
-                        while (!haveWinner.get() && localCount != 0) {
-                            try {
-                                Thread.sleep(pause.get());
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                            localCount--;
-                        }
-                        if (haveWinner.compareAndSet(false, true)) {
-                            nameOfWinner = Thread.currentThread().getName();
-                        }
-                    }
-                }
-        );
-        Thread third = new Thread(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        pause.set(1);
-                        int localCount = COUNT;
-                        while (!haveWinner.get() && localCount != 0) {
-                            try {
-                                Thread.sleep(pause.get());
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                            localCount--;
-                        }
-                        if (haveWinner.compareAndSet(false, true)) {
-                            nameOfWinner = Thread.currentThread().getName();
-                        }
-                    }
-                }
-        );
-        first.start();
-        second.start();
-        third.start();
-        try {
-            first.join();
-            second.join();
-            third.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+                    )
+            );
         }
-
-
-        System.out.println(nameOfWinner);
+        for (Thread oneThread : threadList) {
+            oneThread.start();
+        }
+        for (Thread oneThread : threadList) {
+            try {
+                oneThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Победитель: " + nameOfWinner);
     }
 }
